95eb08f576d36864c76bf8d689494e23d2f587e0
DiegoFerneyUsedaDiaz <dfusedad@gmail.com>
2025-08-28 02:45:07 -0500

    creacion templates de todos los menus, creacion urls

--- sistema/urls.py
+++ sistema/urls.py
@@ -6,4 +6,7 @@ app_name = 'sistema'
 urlpatterns = [     
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('computer/', TemplateView.as_view(template_name='computer.html'), name='computer'),
+    path('repuestos/', TemplateView.as_view(template_name='repuestos.html'), name='repuestos'),
+    path('accesorios/', TemplateView.as_view(template_name='accesorios.html'), name='accesorios'),
+    path('informes/', TemplateView.as_view(template_name='informes.html'), name='informes'),
     ]
